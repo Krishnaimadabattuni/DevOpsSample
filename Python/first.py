3ef379c5de3574cf080348db323b51d286845779
Krishna <krishna.imadabattuni@outlook.com>
2020-10-02 21:51:40 +0530

    practice02102020

--- Python/first.py
+++ Python/first.py
@@ -1,1 +1,15 @@
 print("hello World") 
+
+
+print("sunday" + "Tuesday")
+
+a = 10 + 5j
+b = 20 + 5j
+print(a + b)
+
+fname = "Murali"
+lname = "Krishna"
+
+print(fname + lname)
+
+print(fname[1] + lname[0])
